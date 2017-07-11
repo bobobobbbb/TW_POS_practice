5b5df62826d11bf35eb2995de40365bb2452759d
bobobobbbb <1105388289@qq.com>
2017-07-11 15:13:27 +0800

    pos_v1完成

--- pos_v1/main/main.js
+++ pos_v1/main/main.js
@@ -19,9 +19,12 @@ const tags = [
 
 function printReceipt(tags) {
    var itemsArray = createItemsList(tags) 
-   
+   var itemsArrayPrivilege = dealWithPrivilege(itemsArray)
+   showTable(itemsArrayPrivilege)
 }
 
+function showTable(itemsArrayPrivilege){
+}
 
 function createItemsList(inputs){
   var goodsInformation = loadAllItems;
@@ -58,11 +61,11 @@ function createItemsList(inputs){
   if (barcode.length !== count.length) {
       throw new Error("barcode的长度与count的长度不相等")
   }
-  for (let i = 0; i < barcode.length; i++) {
-    var curObj = new Object()
-    curObj['barcode'] = barcode
-    curObj['count'] = count
-    resArray.push(curOBj)
+  for (let i = 0; i < count.length; i++) {
+    let curObj = new Object()
+    curObj.barcode = barcode[i]
+    curObj.count = count[i]
+    resArray.push(curObj)
   }
   return resArray
 }
@@ -70,41 +73,50 @@ function createItemsList(inputs){
 //传入参数：创建好的array对象
 //返回值：处理好的优惠array对象
 function dealWithPrivilege(inputs) {
+    var resArray = new Array()
     for (let i = 0; i < inputs.length; i++) {
-        if (loadPromotions[0].barcodes.indexOf(inputs[i].barcode) != -1 ) { //如果当前项目存在于优惠列表中
-            
-        } else {
-            
+        let curBarcode = inputs[i].barcode      
+        let curCount = inputs[i].count                     //当前遍历到的barcode
+        if (loadPromotions[0].barcodes.indexOf(curBarcode) != -1 ) { //如果当前barcodes存在于优惠列表中
+            inputs[i].less = calculateLess(curCount)
+        } else { //如果当前barcodes存在于优惠列表中
+            inputs[i].less = 0
         }
     }
-
+    return inputs
 }
 
-
-
-
-function printReceipt(input) {
-    itemsList = new Object()
-    var expectText = '';
-    for (let index = 0; index < inputs.length; index++) {
-        if (inputs[index] in itemsList) {itemsList[inputs[index]] += 1;}
-        else {itemsList[inputs[index]] = 1;}
-    }
-
-    expectText += '***<没钱赚商店>购物清单***\n' 
-    var sumPrice = 0;
-    var allItems = loadAllItems()
-    for (var keyname in itemsList) {
-        var index = parseInt(keyname.substring(4))
-        var currentObj = allItems[index]
-        var totalPrice = itemsList[keyname] * currentObj.price
-        console.log(currentObj.unit)
-        sumPrice += totalPrice
-        expectText += '名称：'+ currentObj.name + '，数量：' + itemsList[keyname] + currentObj.unit + 
-        '，单价：' + currentObj.price.toFixed(2) + '(元)，小计：' + totalPrice.toFixed(2) + '(元)\n'
-    }
-    expectText += "----------------------\n"
-    expectText += "总计：" + sumPrice.toFixed(2) + '(元)\n' 
-    expectText += '**********************'
-    return expectText
+function calculateLess(count) {
+    var less = parseInt(count / 3)
+    return less
 }
+
+function showTable(inputs) {
+    console.log(`***<没钱赚商店>收据***\n`)
+    var totalPrice = 0
+    var saveMoney = 0
+    for (let i = 0; i < inputs.length; i++) {
+        let indexOfLoadAllItem = findIndexFromLoadAllItem(inputs[i].barcode)
+        let curItem = loadAllItems[indexOfLoadAllItem]
+        let curItemTotalPrice = curItem.price * (inputs[i].count - inputs[i].less)
+        totalPrice += curItemTotalPrice
+        saveMoney += curItem.price * inputs[i].less
+        console.log(`名称：${curItem['name']}，数量：${inputs[i].count}${curItem['unit']}，单价：${curItem['price'].toFixed(2)}(元)，小计：${curItemTotalPrice.toFixed(2)}(元)\n`)
+    }
+    console.log('----------------------\n')
+    console.log(`总计：${totalPrice.toFixed(2)}\n`)
+    console.log(`节省：${saveMoney.toFixed(2)}\n`)
+    
+}
+
+function findIndexFromLoadAllItem(barcode) {
+    for (let i = 0; i < loadAllItems.length; i++) {
+        if (loadAllItems[i].barcode == barcode) {
+            return i
+        }
+    }
+    return null
+}
+
+//单一脚本运行
+printReceipt(tags)
